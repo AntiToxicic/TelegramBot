f053e9740987544c111e4d12d7d60c32af62fced
AntiToxic-PC <truhin.1999@mail.ru>
2023-09-02 18:58:03 +0700

    Работа на ботом. Разбор базовых функций.
    1. Сделана функция загрузки картинки
    2. Сделана функция отправки картинки
    3. Сделана функция клавиатурного меню

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,45 +1,194 @@
-﻿
-using System.Net;
-using System.Text;
-using Newtonsoft.Json.Linq;
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults; //inline menu
 
-Thread task = new(BackgrundBot);
-task.Start();
 
-static void BackgrundBot()
+HttpClient httpClient = new();
+
+
+string token = System.IO.File.ReadAllText("token.txt");
+
+var botClient = new TelegramBotClient(token, httpClient);
+
+string[] sites = { "Google", "Github", "Telegram", "Wikipedia" };
+string[] siteDescriptions =
 {
-    string token = File.ReadAllText(@"/Users/antitoxic/Pet_Projects/TelegramBot/token.txt");
-        
-    WebClient web = new() { Encoding = Encoding.UTF8 };
+    "Google is a search engine",
+    "Github is a git repository hosting",
+    "Telegram is a messenger",
+    "Wikipedia is an open wiki"
+};
 
-    int update_id = 0;
-    string startUrl = $@"https://api.telegram.org/bot{token}/";
+string answer = "null";
 
-    while(true)
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync
+);
+
+
+
+
+
+
+
+
+// StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+
+
+
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+// Send cancellation request to stop bot
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    var chatId = update.Message.Chat.Id;
+
+    
+
+    if(update.Message.Text == "/complain" || answer == "/complain")
     {
-        string url = $"{startUrl}getUpdates?offset={update_id}";
-        var c = web.DownloadString(url);
+        answer = "/complain";
 
-         var msgs = JObject.Parse(c)["result"].ToArray();
-
-        foreach(dynamic e in msgs)
-        {
-            update_id = Convert.ToInt32(e.update_id) + 1;
-
-            string userMessage = e.message.text;
-            string userId = e.message.from.id;
-            string useFirstrName = e.message.from.first_name;
-
-            string text = $"{useFirstrName} {userId} {userMessage}";
-
-            if (userMessage == "hi")
+        botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "1. Послать вас нахуй?\n2. Бот идет нахуй\n3. Назад"
+        );
+        if(update.Message.Text != "/complain")
+        {    switch (update.Message.Text)
             {
-                string responseText = $"Вы написали, \"{userMessage}\"";
-                url = $"{startUrl}sendMessage?chat_id={userId}&text={responseText}";
-                web.DownloadString(url);
-            }
-        }
+                case "1": 
+                    await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Иди нахуй!"
+            );
+                break;
 
-        Thread.Sleep(10);
+                case "2": 
+                    await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Сам иди нахуй!"
+            );
+                break;
+
+                case "3": 
+                    await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Пошел нахуй, терпила!"
+            );
+                break;
+
+                default: 
+                await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Понятно..."
+            );
+                break;
+            }}
+
+            answer = "null";
+            return;
     }
+
+     try
+    {
+        await (update.Type switch
+        {
+            UpdateType.InlineQuery => BotOnInlineQueryReceived(botClient, update.InlineQuery!),
+            
+            _ => Task.CompletedTask
+        });
+    }
+
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Exception while handling {update.Type}: {ex}");
+    }
+
+    // Only process Message updates: https://core.telegram.org/bots/api#message
+    if(update.Message.Photo is {})
+    {
+        var fileId = update.Message.Photo.Last().FileId;
+        var fileInfo = await botClient.GetFileAsync(fileId);
+        var filePath = fileInfo.FilePath;
+
+        string UpNamePhoto = "downloaded Photo.jpg";
+        string path2 = @"C:/Users/Alex/Downloads/";
+
+        await using Stream stream1 = System.IO.File.Create(path2 + UpNamePhoto);
+        await botClient.DownloadFileAsync(
+            filePath: filePath,
+            destination: stream1
+        );
+
+        await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "Спасибо за фоточку"
+        );
+    }
+
+    if (update.Message is not {} message)
+        return;
+    // Only process text messages
+    if (message.Text is not { } messageText)
+        return;
+
+    
+
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+   
+
+
+
+
+    // Echo received message text
+
+    string namePhoto = "photoTest.jpg";
+    string path = @"C:/_Alex/Code/TelegramBot/TelegramBot/";
+    
+    using (Stream stream = new FileStream(path + namePhoto, FileMode.Open))
+    {  
+    
+        Message photo = await botClient.SendPhotoAsync(
+        chatId: chatId,
+        photo: InputFile.FromStream(stream, "photoTest.jpg"),
+        caption: "Фоточка, на не ней луна",
+        parseMode: ParseMode.Html,
+        cancellationToken: cancellationToken);  
+    }
+   
 }
+
+async Task BotOnInlineQueryReceived(ITelegramBotClient bot, InlineQuery inlineQuery)
+{
+    var results = new List<InlineQueryResult>();
+
+    var counter = 0;
+    foreach (var site in sites)
+    {
+        results.Add(new InlineQueryResultArticle(
+            $"{counter}", // we use the counter as an id for inline query results
+            site, // inline query result title
+            new InputTextMessageContent(siteDescriptions[counter])) // content that is submitted when the inline query result title is clicked
+        );
+        counter++;
+    }
+
+    await bot.AnswerInlineQueryAsync(inlineQuery.Id, results); // answer by sending the inline query result list
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    return Task.CompletedTask;
+}
+
+
