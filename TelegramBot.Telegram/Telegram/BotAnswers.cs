908a114b3c0ddf3c6826986f4468e071339fd2e0
AntiToxic-PC <truhin.1999@mail.ru>
2023-10-16 07:43:07 +0700

    Изменение названий
    Дальше -> Следующая
    Сначала загрузите картинку -> Сначала нажмите команду 'Следующая картинка'

--- TelegramBot.Telegram/Telegram/BotAnswers.cs
+++ TelegramBot.Telegram/Telegram/BotAnswers.cs
@@ -4,7 +4,7 @@ public class BotAnswers
 {
     public const string start = "Ну, что все просто. Смотри картинки, смейся, загружай свои и снова.\nПриятного просмотра";
     public const string rules = "Правила:\n\t1. Не загружать дубликаты\n\t2. Смеяться над мемами";
-    public const string wrongPicture = "Сначала отправьте картинку";
+    public const string wrongPicture = "Сначала нажмите команду \"Загрузить картинку\"";
     public const string goodPicture = "Ваша картинка принята";
     public const string waitingPicture = "Ждем вашу картинку";
     public const string noAnswer = "Нет такого ответа";
--- TelegramBot.Telegram/Telegram/BotCommands.cs
+++ TelegramBot.Telegram/Telegram/BotCommands.cs
@@ -5,7 +5,7 @@ public class BotCommands
     public const string start = "/start";
     public const string rules = "/rules";
     public const string UploadPicture = "Загрузить картинку";
-    public const string SwapPicture = "Дальше";
+    public const string SwapPicture = "Следующая";
     
     public const string KeyToReceive = "asdfaUJHBU&^UIG";
     
