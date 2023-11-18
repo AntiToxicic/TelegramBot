9d9dd236655a89b79d16839c25e9efae0b15405f
AntiToxic-PC <truhin.1999@mail.ru>
2023-11-18 13:20:51 +0700

    temp

--- TelegramBot.Api/Controllers/TelegramController.cs
+++ TelegramBot.Api/Controllers/TelegramController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
 using Telegram.Bot.Types;
 
 namespace TelegramBot.Telegram.Controllers;
@@ -7,6 +8,7 @@ namespace TelegramBot.Telegram.Controllers;
 [Route("[controller]")]
 public class TelegramController : ControllerBase
 {
+    private ITelegramBotClient _telegramBotClient;
     public TelegramController()
     {
         
@@ -15,6 +17,17 @@ public class TelegramController : ControllerBase
     [HttpPost("webhook")]
     public async Task<IActionResult> WebHook(Update update)
     {
+        var updates = await _telegramBotClient.GetUpdatesAsync(limit:100);
+        var updateId = update.Message.MessageId;
+        var chatId = update.Message.Chat.Id;
+
+        var previosUpdate = updates.ToList().FirstOrDefault(c => c.Message.MessageId == updateId - 1);
+        var lastMsg = previosUpdate.Message.Text;
+
+        if (lastMsg == ...)
+        {
+            ...
+        }
        
         return Ok();
     }
